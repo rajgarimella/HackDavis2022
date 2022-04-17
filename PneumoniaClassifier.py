bf12f0a4a2158b96d72dc80a0d2480b7511f0a14
profilename14 <44542640+profilename14@users.noreply.github.com>
2022-04-16 23:38:51 -0700

    Add files via upload
    Current (broken)_version of the classifer

---
+++ PneumoniaClassifier.py
@@ -0,0 +1,102 @@
+
+# Created by James Jasper Fadden O'ROarke
+# This is a image classifer built from the Keras python library and based on previous machine learning attempts. It is designed to determine whether a lung scan is suffering from pneumonia or is healthy, serving as a core part of the project. There is logic for the classifer framework, training the classifer, and saving/loading for use. 
+
+import tensorflow
+import keras
+from keras.layers import Dense
+from keras.layers import Input
+from keras.layers import Dropout
+from keras.layers import Conv2D
+from keras.layers import Flatten
+from keras.layers import MaxPool2D
+from keras.models import Sequential
+import numpy as np
+
+model = Sequential()
+
+trainData = tensorflow.keras.preprocessing.image_dataset_from_directory(
+    directory='TrainingData/',
+    labels='inferred',
+    label_mode='int',
+    color_mode="grayscale",
+    batch_size=5,
+    image_size=(64, 64))
+testData = tensorflow.keras.preprocessing.image_dataset_from_directory(
+    directory='TestingData/',
+    labels='inferred',
+    label_mode='int',
+    color_mode="grayscale",
+    batch_size=5,
+    image_size=(64, 64))
+
+
+#trainDataImage = np.concatenate([ x for x, y in trainData ], axis=0)
+#trainDataLabel = np.concatenate([ y for x, y in trainData ], axis=0)
+
+#testDataImage  = np.concatenate([ x for x, y in testData  ], axis=0)
+#testDataLabel  = np.concatenate([ y for x, y in testData  ], axis=0)
+
+
+model.add(Conv2D(60, kernel_size = 1, activation='relu', input_shape=(64, 64, 1), padding='same'))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(35, kernel_size = 1, activation='relu', padding='same'))
+model.add(Dropout(0.2))
+
+model.add(MaxPool2D(2, 2))
+
+model.add(Conv2D(20, kernel_size = 1, activation='relu', padding='same'))
+model.add(Dropout(0.2))
+
+model.add(MaxPool2D(2, 2))
+
+model.add(Conv2D(10, kernel_size = 1, activation='relu', padding='same'))
+model.add(Dropout(0.2))
+
+model.add(Flatten())
+
+model.add(Dense(2, activation='softmax'))
+
+model.summary()
+
+def Reshaper(var, imNumber, isImage):
+    if(isImage==True):
+        np.reshape(var, (imNumber, 64, 64, 1))
+    else:
+        np.reshape(var, (imNumber, 1))
+    return var
+
+#trainDataImage = Reshaper(trainDataImage, 500, True)
+#trainDataLabel = Reshaper(trainDataLabel, 500, False)
+
+#np.reshape(trainData, (1, 500, 64, 64, 1))
+
+def OneHotEncode(DataLabel, labelNum):
+    OneHot = np.zeros((labelNum, 2))
+    count=0
+
+    while (count < labelNum):
+        OneHot[count][DataLabel[count].astype(int)] = 1
+        count=count+1
+
+    return OneHot
+
+#trainDataLabel = OneHotEncode(trainDataLabel, 500)
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(trainData, epochs=25, batch_size=5)
+
+#testDataImage = Reshaper(testDataImage, 500, True)
+#testDataLabel = Reshaper(testDataLabel, 500, False)
+#testDataLabel = OneHotEncode(testDataLabel, 500)
+#np.reshape(testData, (500, 64, 64, 1, 1))
+
+output = model.evaluate(testData, verbose=True, batch_size=5)
+
+print("The model loss and accuracy respectively:", output)
+
+model.save('PneumoniaModel.h5')
+
+
